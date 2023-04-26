f70b9683347eb8ad426456ecc110627f0ef2fcda
MSA2001 <msa2001b21@gmail.com>
2023-04-26 15:59:25 +0330

    Ads Detail view

--- ads/urls.py
+++ ads/urls.py
@@ -6,4 +6,5 @@ app_name = 'ads'
 urlpatterns = [
     path('list/', views.AdListView.as_view()),
     path('create/', views.AdCreateView.as_view()),
+    path('detail/<int:pk>/', views.AdDetailView.as_view()),
 ]
