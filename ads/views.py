64059c1d5bd85975071a1d353eaf85e7c7ac6cc9
MSA2001 <msa2001b21@gmail.com>
2023-04-26 17:04:29 +0330

    Ads delete & update

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from .models import Ad
 from .serializers import AdSerializer
@@ -6,6 +7,7 @@ from rest_framework import status
 from .Pagination import StandardResultSetPagination
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsPublisherOrReadOnly
 # Create your views here.
 
 
@@ -35,8 +37,28 @@ class AdCreateView(APIView):
 
 class AdDetailView(APIView):
     serializer_class = AdSerializer
+    permission_classes = (IsAuthenticated, IsPublisherOrReadOnly)
+    parser_classes = (MultiPartParser,)
+
+    def get_object(self, pk):
+        obj = get_object_or_404(Ad.objects.all(), id=self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+        return obj
 
     def get(self, request, pk):
-        instance = Ad.objects.get(id=pk)
+        instance = self.get_object()
         serializer = AdSerializer(instance=instance)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request,pk):
+        instance = self.get_object()
+        serializer = AdSerializer(data=request.data, instance=instance, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        instance = self.get_object()
+        instance.delete()
+        return Response({'response': 'Ad deleted'})
